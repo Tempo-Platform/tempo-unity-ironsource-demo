50f1b3ad4845601029cc301ea7c3fc6392aa660f
omnibaker <37693234+omnibaker@users.noreply.github.com>
2023-06-06 16:12:40 +1000

    Working load and reload with Android

--- Assets/TempoCanvas.cs
+++ Assets/TempoCanvas.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System.Runtime.CompilerServices;
 
 public class TempoCanvas : MonoBehaviour
 {
@@ -13,50 +14,45 @@ public class TempoCanvas : MonoBehaviour
     public Button InterstitialPlay;
     public TextMeshProUGUI InterstitialMsg;
 
-
     public static TempoCanvas Instance;
 
     private void Awake()
     {
         Instance = this;
-        InterstitialLoad.onClick.AddListener(OnInterstitialLoad);
-        InterstitialPlay.onClick.AddListener(OnInterstitialPlay);
-        RewardedLoad.onClick.AddListener(OnRewardedLoad);
-        RewardedPlay.onClick.AddListener(OnRewardedReady);
+        InterstitialLoad.onClick.AddListener(LoadInterstitialAd);
+        InterstitialPlay.onClick.AddListener(ShowInterstitialAd);
+        RewardedLoad.onClick.AddListener(LoadRewardedAd);
+        RewardedPlay.onClick.AddListener(ShowRewardedlAd);
     }
 
-    public void OnInterstitialLoad()
+    private void LoadInterstitialAd()
     {
         InterstitialMsg.text = "Loading...";
     }
-
-    public void OnInterstitialReady()
+    private void ShowInterstitialAd()
+    {
+        InterstitialMsg.text = "";
+        InterstitialPlay.interactable = false;
+    }
+    public void EnableInterstitialAd()
     {
         InterstitialMsg.text = "";
         InterstitialPlay.interactable = true;
     }
 
-    public void OnInterstitialPlay()
-    {
-        InterstitialMsg.text = "";
-        InterstitialPlay.interactable = false;
-    }
 
-
-    public void OnRewardedLoad()
+    private void LoadRewardedAd()
     {
         RewardedMsg.text = "Loading...";
     }
-
-    public void OnRewardedReady()
-    {
-        RewardedMsg.text = "";
-        RewardedPlay.interactable = true;
-    }
-
-    public void OnRewardedPlay()
+    private void ShowRewardedlAd()
     {
         RewardedMsg.text = "";
         RewardedPlay.interactable = false;
+    }
+    public void EnableRewardedAd()
+    {
+        RewardedMsg.text = "";
+        RewardedPlay.interactable = true;
     }
 }
